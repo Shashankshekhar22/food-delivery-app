365f2f426fa58633168a3b9410aae237d6cd7f66
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2023-08-20 12:55:29 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(Render)
+// React Element
+
+/*
+ * Food App Design
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Searchh
+ *  - ResturantConyainer
+ *    -- ResturantCard
+ * Footer
+ *  -- Copyright
+ *  -- Links
+ *  -- Address
+ *  --Contact
+ *  -- Any add ons
+ */
+
+const jsxHeading = <h1 id="heading">This is my React Element 🚀 </h1>;
+
+const Title = () => {
+  return <h1 className="titleComponent">This is the Title Component</h1>;
+};
+// React Component
+// Component composition: Clubbing of 2 or more componnet together
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {jsxHeading}
+      <h3>Called as a function: </h3> {Title()}
+      <Title />
+      <h1 className="heading">This is a react Functional Component</h1>
+    </div>
+  );
+};
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
