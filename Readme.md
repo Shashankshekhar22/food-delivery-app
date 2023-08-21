36f80028f5bcc13aed995e5bf6e0b2e5964eab8d
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2023-08-21 22:15:25 +0530

    useState concept implementation and virtual dom details

--- Readme.md
+++ Readme.md
@@ -2,3 +2,17 @@
 
 - export const abc this requires curly braces while importing
 - default export does not requires curly braces while importing
+
+# React Hooks
+
+- Normal JS utility function
+- useState() - Super powerful state variable
+- whenever state variable is updated react is rerendering the component
+- useEffects()
+
+- Reconciliation Algorithm (React Fiber-->Introduced in React 16)
+
+- Virtual Dom is a representation of Actual DOM which is a React Element i.e. an nested Object of all the DOM elements
+
+- Diff Algorithm
+- It finds the difference between virtual DOM and updated DOM
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,35 @@
 import RestaurantCard from "../components/ResturantCard";
 import { swiggyMockData } from "../utils/swiggyMockData";
+import { useState } from "react";
 
 const resData = swiggyMockData;
 
 const Body = () => {
-  const resListData =
-    resData.data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
+  // State Variable - Super powerful Variable
+  const [resListData, setResListData] = useState([
+    ...resData.data.cards[2].card.card.gridElements.infoWithStyle.restaurants,
+  ]);
+  // Normal JS variable
+  //   let resListData =
+  //     resData.data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
   return (
     <div className="body">
-      <div className="search">Search</div>
-      <div className="resturant-container">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = resListData.filter((restaurantData) => {
+              return restaurantData.info.avgRating > 4;
+            });
+            setResListData(filteredList);
+
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="restaurant-container">
         {resListData.map((restaurantData) => (
           <RestaurantCard
             key={restaurantData.info.id}
